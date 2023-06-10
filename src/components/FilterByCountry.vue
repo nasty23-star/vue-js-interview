0759a3123a44bfb09f9245d431ec8e09d8fecfc8
dimnas <fysyad@gmail.com>
2023-06-10 17:05:49 +0300

    filters, store

---
+++ src/components/FilterByCountry.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container class="container">
+    <v-select
+      input
+      type="text"
+      :value="searchByCountry"
+      @input="setSearch"
+      :items="countrys"
+      clearable
+      label="Filter by country"
+    ></v-select>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "FilterByCountry",
+
+  data: () => ({
+    variant: "default",
+  }),
+  computed: {
+    searchByCountry() {
+      return this.$store.state.searchByCountry;
+    },
+    user() {
+      return this.$store.state.users.user;
+    },
+    countrys() {
+      return this.$store.getters.countrys;
+    },
+    users() {
+      return this.$store.state.users;
+    },
+  },
+  methods: {
+    setSearch(e) {
+      this.$store.commit("setSearch", e);
+    },
+  },
+};
+</script>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,137 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+// import axios from "axios"; раскомментировать, чтобы использовать аксиос для запроса на сервер
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import Vuex from "vuex";
+Vue.config.productionTip = false;
+Vue.use(Vuex);
+const store = new Vuex.Store({
+  state() {
+    return {
+      searchByCountry: "",
+      searchByScore: "",
+      countrys: ["USA", "Russia"],
+      scores: [">20", "<10"],
+      users: [
+        { header: "List" },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+          title: "Brunch this weekend?",
+          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+          country: `USA`,
+          isShow: false,
+          score: 5,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+          country: `USA`,
+          isShow: false,
+          score: 25,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+          title: "Oui oui",
+          subtitle:
+            '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+          country: `Russia`,
+          isShow: false,
+          score: 7,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+          title: "Birthday gift",
+          subtitle:
+            '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+          country: `Russia`,
+          isShow: false,
+          score: 22,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
+          title: "Recipe to try",
+          subtitle:
+            '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+          country: `Russia`,
+          isShow: false,
+          score: 15,
+        },
+      ],
+    };
+  },
+  actions: {
+    // если раскомментировать то идет аксиос запрос на моковый адрес, который возвращает один объект из массива users
+    // fetchUsers({ commit }) {
+    //   const baseURL = "https://dummyjson.com/users/1";
+    //   axios
+    //     .get(baseURL)
+    //     .then((response) => {
+    //       commit("setUsers", response);
+    //     })
+    //     .catch((e) => {
+    //       console.log(e);
+    //     });
+    // },
+  },
+  mutations: {
+    setSearch(state, searchByCountry) {
+      state.searchByCountry = searchByCountry;
+    },
+    setScore(state, searchByScore) {
+      state.searchByScore = searchByScore;
+    },
+    // setUsers(state, user) { добавляет полученный с помощью аксиос объект в общий массив
+    //   state.users.push(user);
+    // },
+  },
+  getters: {
+    getFiltered(state) {
+      if (!state.searchByCountry && !state.searchByScore) {
+        return state.users;
+      }
+      if (state.searchByCountry && !state.searchByScore) {
+        return state.users.filter(
+          (user) => user.country == state.searchByCountry
+        );
+      }
+      if (state.searchByCountry && state.searchByScore) {
+        return state.users
+          .filter((user) => user.country == state.searchByCountry)
+          .filter((user) =>
+            state.searchByScore == ">20"
+              ? user.score > 20
+              : state.searchByScore == "<10"
+              ? user.score < 10
+              : user
+          );
+      }
+      if (!state.searchByCountry && state.searchByScore) {
+        return state.users.filter((user) =>
+          state.searchByScore == ">20"
+            ? user.score > 20
+            : state.searchByScore == "<10"
+            ? user.score < 10
+            : user
+        );
+      }
+    },
+    countrys(state) {
+      return state.countrys;
+    },
+    scores(state) {
+      return state.scores;
+    },
+  },
+});
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  store: store,
+  // store,
+  render: (h) => h(App),
+}).$mount("#app");
